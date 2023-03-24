de9a12b27538b2b580066715177032ffa28e9ebb
Archana Deulkar <88867952+sawantarchna@users.noreply.github.com>
2023-03-24 13:14:09 -0400

    Second Commit - Added dataProvider, HashMap and Purchase.xml to check Suite level test(groups)

--- SeleniumFrameworkDesignScratch_Base_Global_TestNG_ARC/src/test/java/rahulshettyacademy/Tests/PlaceOrderTest.java
+++ SeleniumFrameworkDesignScratch_Base_Global_TestNG_ARC/src/test/java/rahulshettyacademy/Tests/PlaceOrderTest.java
@@ -4,6 +4,7 @@ import java.awt.Desktop.Action;
 import java.io.IOException;
 import java.security.PublicKey;
 import java.time.Duration;
+import java.util.HashMap;
 import java.util.List;
 
 import org.checkerframework.framework.qual.FromByteCode;
@@ -17,6 +18,7 @@ import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -36,17 +38,17 @@ public class PlaceOrderTest extends BaseTest{
 	
 	String productName = "ZARA COAT 3";
 	
-	@Test
-	public void PlaceOrderTest() throws IOException, InterruptedException{
+	@Test(dataProvider = "getData", groups = "Purchase")//you can put more groups also simply put comma and add it
+	//public void PlaceOrderTest(String email, String password, String productName) throws IOException, InterruptedException{
+	public void PlaceOrderTest(HashMap<String,String>input) throws IOException, InterruptedException{
 		
-		
-		ProductCatalogue productCatalogue = landingPage.loginApplication("sawantarchana110@gmail.com", "Mullen@123");//loginit 
+		ProductCatalogue productCatalogue = landingPage.loginApplication(input.get("email"),input.get("password"));//loginit 
 
 		List<WebElement> products = productCatalogue.getProductList();//getting the product list
-		productCatalogue.addProductToCart(productName);//adding the products to cart
+		productCatalogue.addProductToCart(input.get("productName"));//adding the products to cart
 		
 		CartPage cartPage = productCatalogue.goToCartPage();//going to cart page
-		Boolean match = cartPage.VerifyProductDiaplay(productName);//verifying the product display
+		Boolean match = cartPage.VerifyProductDiaplay(input.get("productName"));//verifying the product display
 		Assert.assertTrue(match);
 	
 		CheckOutPage checkOutPage = cartPage.goToCheckout();//checking out
@@ -71,5 +73,46 @@ public class PlaceOrderTest extends BaseTest{
 	//To verify if that ZARA COAT 3 is displaying in order page
 	//all these are Test Strategy
 	//If you want to run methods in parallel use like this --><suite parallel = 'tests' name="Suite">
+	
+	//2nd method from DataProvider
+	@DataProvider
+	public Object[][] getData()
+	{
+		HashMap<String, String> map = new HashMap<String, String>();
+		map.put("email","sawantarchana110@gmail.com");
+		map.put("password", "Mullen@123");
+		map.put("productName", "ZARA COAT 3");
+
+		HashMap<String, String> map1 = new HashMap<String, String>();
+		map1.put("email","archies1013@gmail.com");
+		map1.put("password", "Mullen@123");
+		map1.put("productName", "ADIDAS ORIGINAL");
+		
+		return new Object [] [] {{map}, {map1} };
+		
+	}
+	//we run this in "Purchase.xml"  file, son run and check
+	//1st - method from DataProvider
+	//@DataProvider
+	//public Object[][] getData()
+	//{
+		//return new Object [] [] {{"sawantarchana110@gmail.com", "Mullen@123", "ZARA COAT 3"}, {"archies1013@gmail.com", "Mullen@123", "ADIDAS ORIGINAL"} };
+		//From Here we converted into "HashMap"
+		//return new Object [] [] {{map}, {"archies1013@gmail.com", "Mullen@123", "ADIDAS ORIGINAL"} };
+		//after add 2nd HashMap = 
+		//return new Object [] [] {{map}, {map1} };
+	//Now we will see How to Drive the data from Json
+	//	}
+	
+	/**Let say if you want to run this above PlaceOrderTest with two different data sets, so simply create two dimensional array
+	 * which accepts multiple sets. 
+	 * Now we have to attach this DataProvider getData test to above PlaceOrdertest and send data as an argument
+	 * Here our code is so generic that we are passing different data sets with the help of dataprovider, so that is how you can Parameterized 
+	 * your test with multiple data sets.
+	 * Next we will see get the data from external sources like excel file
+	 * We created two different data sets with HashMap
+	 */
+	
 
 }
+
